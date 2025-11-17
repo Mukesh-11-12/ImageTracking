6fe8c239b7abf1054a7e1cbbb0c84c87b5bc26c4
Mukesh-11-12 <mukesh779912@gmail.com>
2025-11-17 14:17:34 +0530

    added

---
+++ main.js
@@ -0,0 +1,27 @@
+import { loadGLTF, loadTexture } from "./libs/loader.js";
+
+const THREE = window.MINDAR.FACE.THREE;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const start = async () => {
+    const mindarThree = new window.MINDAR.FACE.MindARThree({
+      container: document.body,
+    });
+
+    const { renderer, scene, camera } = mindarThree;
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    const glasses = await loadGLTF("./asserts/models/glasses1/scene.gltf");
+    glasses.scene.scale.multiplyScalar(0.01);
+    const anchor = mindarThree.addAnchor(168);
+    anchor.group.add(glasses.scene);
+    await mindarThree.start();
+
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  };
+
+  start();
+});
